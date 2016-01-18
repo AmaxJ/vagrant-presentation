ac797a687e34b3cf3fc9124bf80e12b3d3fd1fe3
Alan <ajohnson88@gmail.com>
2016-01-17 23:12:51 -0500

    ok

--- provision.sh
+++ provision.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 apt-get -y update
+apt-get -y install curl
+curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
+apt-get -y install nodejs
+
+echo "source /vagrant/.profile" >> /home/vagrant/.bashrc
+
+cd /vagrant/sample-site
+npm install
+node index.js
