828c0f2793092cdc2c663c82e3efc2b203280276
Alan <ajohnson88@gmail.com>
2016-01-18 22:44:15 -0500

    tweaked example app

--- sample-site/index.js
+++ sample-site/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('<h1>Hello Vagrant!</h1>');
 });
 
 app.listen(8000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 8000!');
 });
